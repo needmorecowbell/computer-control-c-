62c3ff90be31d49db9e3e84989ba95b06b9dcff0
needmorecowbell <amusciano@gmail.com>
2015-10-06 14:06:22 -0400

    Finished changeDirectory method

--- Source.cpp
+++ Source.cpp
@@ -15,16 +15,12 @@ int main(){
 	}
 
 	cout << "Current directory: " << oldDir << '\n';
-	/*chdir("c:");
-	//system("C:");
-	//system("cd V:\Desktop");
-	//system("dir");
-	system("cd C:\Program Files (x86)\Internet Explorer");
-	system("dir");
-	system("iexplore.exe pandora.com");
-	system("pause");*/
-	ChangeDirectory("C:/Program Files (x86)/Internet Explorer");
+	
 
+	ChangeDirectory("C:/Program Files (x86)/Internet Explorer");
+	system("iexplore.exe pandora.com");
+	ChangeDirectory(oldDir);
+	system("pause");
 	int c;
 	cin >> c;
 
@@ -35,31 +31,12 @@ int main(){
 
 
 int ChangeDirectory(const char *url){
-	const int bufferSize = MAX_PATH;
-	char oldDir[bufferSize]; // store the current directory
-
-	// get the current directory, and store it
-	if (!GetCurrentDirectory(bufferSize, oldDir)) {
-		cerr << "Error getting current directory: #" << GetLastError();
-		return 1; // quit if it failed
-	}
-	cout << "Current directory: " << oldDir << '\n';
-	//string urlNew = "("+url+")";
-	// new directory
 	const char* newDir = R"(C:/Program Files (x86)/Internet Explorer)";
 	if (!SetCurrentDirectory(url)) {
 		cerr << "Error setting current directory: #" << GetLastError();
 		return 1; // quit if we couldn't set the current directory
 	}
 	cout << "Set current directory to " << newDir << '\n';
-
-	system("dir");
-	// Reset the current directory back to what it was.
-	if (!SetCurrentDirectory(oldDir)) {
-		cerr << "Error resetting current directory: #" << GetLastError();
-		return 1;
-	}
-	cout << "Reset current directory. \n";
 
 }
 void LeftClick(){
